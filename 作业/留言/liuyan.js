702c7dfab710638e74a7ad358132ec23f02be39b
songhaohahaha <31531698+songhaohahaha@users.noreply.github.com>
2017-09-25 22:57:13 +0800

    Update liuyan.js

--- 作业/留言/liuyan.js
+++ 作业/留言/liuyan.js
@@ -8,14 +8,13 @@ window.onload = function(){
 	let liuyanTop = document.getElementsByClassName('liuyan-top')[0];
 	let liuyanBot = document.getElementsByClassName('liuyan-bottom')[0];
 	let textTop = liuyanTop.getElementsByClassName('wen1')[0];
-	let textBot = liuyanBot.getElementsByTagName('span')[0];
+	// let textBot = liuyanBot.getElementsByTagName('span')[0];
 	let btn = document.getElementsByTagName('button')[0];
-
 	btn.onclick = function(){
-		let texts = document.createElement('textarea');
-		texts.style.cssText = `width:100%; border:1px solid orange;`;
+		let texts = document.createElement('p');
+		texts.style.cssText = `width:100%;height:50px; border:1px solid orange;`;
 		liuyanBot.prependChild(texts);
-		texts.value = textTop.value;
+		texts.innerHTML = textTop.value;
 		textTop.value = '';
 	}
 }
